2d55a446fa1c03b1fd87e8925f93c3906161f25d
MattZK <mattwillemsen@icloud.com>
2018-12-06 10:14:42 +0100

    Initial Commit /w Unit Test

---
+++ app/src/main/java/be/mattwill/dev/myfancytestapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package be.mattwill.dev.myfancytestapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/be/mattwill/dev/myfancytestapplication/Utils.java
@@ -0,0 +1,7 @@
+package be.mattwill.dev.myfancytestapplication;
+
+public class Utils {
+  public static int addNumbers(int first, int second) {
+    return first + second;
+  }
+}
